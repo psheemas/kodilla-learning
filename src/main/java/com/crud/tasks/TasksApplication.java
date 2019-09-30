470aa196e0a742aff2203e5dd988ddc618ca6445
psheemas <kulakowski.przemyslaw@gmail.com>
2019-09-30 12:50:17 +0200

    34.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication
-        //extends SpringBootServletInitializer
-{
+public class TasksApplication extends SpringBootServletInitializer {
 //public class TasksApplication{
 
         public static void main(String[] args) {
